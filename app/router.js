f362e80cc0c0d3bf826de2bdb2a6bbaee918a5f5
ScottWallin <scott.wallin@me.com>
2023-05-17 15:05:43 -0600

    started

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import { AboutController } from "./controllers/AboutController.js";
+import { HomeController } from "./controllers/HomeController.js";
+
+export const router = [
+  {
+    path: '',
+    controller: HomeController
+  },
+
+]
